ef053a686e3b8bac7cd45f46bfcde85e90bc66cb
Adrian Rodriguez Calderon <adroca@badia.cat>
2023-03-30 16:49:30 +0200

    migrations

---
+++ Componente/database/migrations/2023_03_30_144650_create_carritos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carritos', function (Blueprint $table) {
+            $table->id('idCarrito');
+            $table->string('NombreP');
+            $table->integer('Precio');
+            $table->foreignId('idUsuario')->constrained('users');
+            $table->string('Imagen');
+            $table->integer('Cantidad')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carritos');
+    }
+}
